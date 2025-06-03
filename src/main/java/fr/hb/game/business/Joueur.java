baef8e0f092b1fa2048e411363f94cef0f5162e7
Anael Payet <anaelpayetpro@gmail.com>
2025-06-03 10:03:11 +0200

    ✨ feat : Add relation into Joueur and Moderateur

--- src/main/java/fr/hb/game/business/Joueur.java
+++ src/main/java/fr/hb/game/business/Joueur.java
@@ -1,11 +1,14 @@
 package fr.hb.game.business;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Joueur extends Utilisateur {
   
   private LocalDate dateNaissance;
-  
+  private List<Avis> avisList = new ArrayList<>();
+
   public Joueur(String pseudo, String motDePasse, String email, LocalDate dateNaissance, Avatar avatar) {
     super(pseudo, motDePasse, email, avatar);
     this.dateNaissance = dateNaissance;
@@ -17,6 +20,14 @@ public class Joueur extends Utilisateur {
   
   public void setDateNaissance(LocalDate dateNaissance) {
     this.dateNaissance = dateNaissance;
+  }
+
+  public List<Avis> getAvisList() {
+    return avisList;
+  }
+
+  public void setAvisList(List<Avis> avisList) {
+    this.avisList = avisList;
   }
 
   @Override
--- src/main/java/fr/hb/game/business/Moderateur.java
+++ src/main/java/fr/hb/game/business/Moderateur.java
@@ -1,10 +1,12 @@
 package fr.hb.game.business;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Moderateur extends Utilisateur {
   
   private String numeroDeTelephone;
-
+  private List<Avis> avisList = new ArrayList<>();
 
   public Moderateur(String pseudo, String email, String motDePasse,String numeroDeTelephone, Avatar avatar) {
     super(pseudo, email, motDePasse, avatar);
@@ -17,6 +19,14 @@ public class Moderateur extends Utilisateur {
 
   public void setNumeroDeTelephone(String numeroDeTelephone) {
     this.numeroDeTelephone = numeroDeTelephone;
+  }
+
+  public List<Avis> getAvisList() {
+    return avisList;
+  }
+
+  public void setAvisList(List<Avis> avisList) {
+    this.avisList = avisList;
   }
 
   public String toString() {
